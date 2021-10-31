bbf1f54d465cda4925f002e4707d3292291d6125
Szymon Murawski <szymon.murawski@gmail.com>
2021-10-31 15:26:29 +0100

    feat: Initialize cells neighbors

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -14,6 +14,15 @@ public class Grid {
                 CellsGrid[x][y] = new Cell(x, y);
             }
         }
+        for (int x=0; x< Width; x++){
+            for (int y=0; y< Height; y++){
+                Cell cell = CellsGrid[x][y];
+                cell.North = cell.Y - 1 < 0 ? null : CellsGrid[cell.X][cell.Y-1];
+                cell.South = cell.Y + 1 >= Height ? null : CellsGrid[cell.X][cell.Y + 1];
+                cell.West = cell.X - 1 < 0 ? null : CellsGrid[cell.X - 1][cell.Y];
+                cell.East = cell.X + 1 >= Width ? null : CellsGrid[cell.X + 1][cell.Y];
+            }
+        }
     }
 }
 
--- test/com/company/GridTest.java
+++ test/com/company/GridTest.java
@@ -12,5 +12,10 @@ public class GridTest {
         Assert.assertEquals("CellsGrid width is not properly set", 10, grid.CellsGrid.length);
         Assert.assertEquals("CellsGrid height is not properly set", 11, grid.CellsGrid[0].length);
         Assert.assertNotNull("Cell is null", grid.CellsGrid[0][0]);
+        Assert.assertNotNull("North neighbor is null", grid.CellsGrid[1][1].North);
+        Assert.assertNotNull("South neighbor is null", grid.CellsGrid[1][1].South);
+        Assert.assertNotNull("East neighbor is null", grid.CellsGrid[1][1].East);
+        Assert.assertNotNull("West neighbor is null", grid.CellsGrid[1][1].West);
     }
 }
+
