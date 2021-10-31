cbf6ee1551c22c55f1decd2c791fd2b844b77ad1
Szymon Murawski <szymon.murawski@gmail.com>
2021-10-31 13:08:55 +0100

    feat: Initialize every cell

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -9,6 +9,11 @@ public class Grid {
         Width = width;
         Height = height;
         CellsGrid = new Cell[Width][Height];
+        for (int x=0; x< Width; x++){
+            for (int y=0; y< Height; y++){
+                CellsGrid[x][y] = new Cell(x, y);
+            }
+        }
     }
 }
 
--- test/com/company/GridTest.java
+++ test/com/company/GridTest.java
@@ -11,6 +11,6 @@ public class GridTest {
         Assert.assertEquals("Height is not properly set", 11, grid.Height);
         Assert.assertEquals("CellsGrid width is not properly set", 10, grid.CellsGrid.length);
         Assert.assertEquals("CellsGrid height is not properly set", 11, grid.CellsGrid[0].length);
-
+        Assert.assertNotNull("Cell is null", grid.CellsGrid[0][0]);
     }
 }
