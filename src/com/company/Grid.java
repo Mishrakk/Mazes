770dc2fbe3abbb57144945d244f4a48c4cf58785
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-02 18:54:47 +0100

    refactor: Use GetCellAt() to set neighbours

--- src/com/company/Grid.java
+++ src/com/company/Grid.java
@@ -17,10 +17,10 @@ public class Grid {
         for (int x=0; x< Width; x++){
             for (int y=0; y< Height; y++){
                 Cell cell = CellsGrid[x][y];
-                cell.North = cell.Y - 1 < 0 ? null : CellsGrid[cell.X][cell.Y-1];
-                cell.South = cell.Y + 1 >= Height ? null : CellsGrid[cell.X][cell.Y + 1];
-                cell.West = cell.X - 1 < 0 ? null : CellsGrid[cell.X - 1][cell.Y];
-                cell.East = cell.X + 1 >= Width ? null : CellsGrid[cell.X + 1][cell.Y];
+                cell.North = GetCellAt(cell.X,cell.Y-1);
+                cell.South = GetCellAt(cell.X, cell.Y + 1);
+                cell.West = GetCellAt(cell.X - 1, cell.Y);
+                cell.East = GetCellAt(cell.X + 1, cell.Y);
             }
         }
     }
