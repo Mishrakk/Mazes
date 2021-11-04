5ea41585022cbf7f86f641ef468f672c8ae8e161
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-04 21:42:53 +0100

    refactor: Extract getNeighbours() in binary tree generator

--- src/com/company/BinaryTreeMaze.java
+++ src/com/company/BinaryTreeMaze.java
@@ -1,28 +1,29 @@
 package com.company;
 
-import java.util.LinkedList;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class BinaryTreeMaze {
-    public static void Generate(Grid grid)
-    {
+    public static void Generate(Grid grid) {
         Random rand = new Random();
-        for (Cell cell : grid){
-            LinkedList<Cell> neighbours = new LinkedList<>();
-            if (cell.North != null)
-            {
-                neighbours.add(cell.North);
-            }
-            if (cell.East != null)
-            {
-                neighbours.add(cell.East);
-            }
+        for (Cell cell : grid) {
+            List<Cell> neighbours = getNeighbours(cell);
 
-            if (neighbours.size() > 0)
-            {
+            if (neighbours.size() > 0) {
                 Cell neighbour = neighbours.get(rand.nextInt(neighbours.size()));
                 cell.LinkCell(neighbour);
             }
         }
+    }
+    private static List<Cell> getNeighbours(Cell cell) {
+        List<Cell> neighbours = new ArrayList<>();
+        if (cell.North != null) {
+            neighbours.add(cell.North);
+        }
+        if (cell.East != null) {
+            neighbours.add(cell.East);
+        }
+        return neighbours;
     }
 }
