90a8f0a5a0c257847dc441573fb6de57c969f554
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-07 16:21:21 +0100

    refactor: Default cell size

--- src/com/company/MazePrinter.java
+++ src/com/company/MazePrinter.java
@@ -4,11 +4,14 @@ import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class MazePrinter {
-    private Grid Grid;
-    private int CellSize;
+    private final Grid Grid;
+    private final int CellSize;
     public MazePrinter(Grid grid, int cellSize){
         Grid = grid;
         CellSize = cellSize;
+    }
+    public MazePrinter(Grid grid){
+        this(grid, 10);
     }
     public BufferedImage GetGraphics(){
         BufferedImage bufferedImage = new BufferedImage(CellSize * Grid.Width, CellSize * Grid.Height, BufferedImage.TYPE_INT_RGB);
--- test/com/company/MazePrinterTest.java
+++ test/com/company/MazePrinterTest.java
@@ -10,8 +10,16 @@ public class MazePrinterTest {
     public void testImageSize() {
         Grid grid = new Grid(2,3);
         MazePrinter mazePrinter = new MazePrinter(grid, 10);
-        var  image = mazePrinter.GetGraphics();
+        var image = mazePrinter.GetGraphics();
         Assert.assertEquals("Image width should be equal to cell size times grid width", 20, image.getWidth());
         Assert.assertEquals("Image height should be equal to cell size times grid height", 30, image.getHeight());
+    }
+    @Test
+    public void testDefaultCellSize() {
+        Grid grid = new Grid(2,3);
+        MazePrinter mazePrinter = new MazePrinter(grid);
+        var image = mazePrinter.GetGraphics();
+        Assert.assertEquals("Image width is based on default cell size", 20, image.getWidth());
+        Assert.assertEquals("Image height is based on default cell size", 30, image.getHeight());
     }
 }
