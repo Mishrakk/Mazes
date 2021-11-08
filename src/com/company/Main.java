e3ffcf00690696900016d76f698ff1b556964ece
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-08 20:39:25 +0100

    chore: Add print to png to Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,11 +1,18 @@
 package com.company;
 
+import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.IOException;
+
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         System.out.print("Hello World!\n");
         Grid grid = new Grid(10, 10);
         SidewinderMaze.Generate(grid);
         System.out.print(grid);
+        MazePrinter mazePrinter = new MazePrinter(grid);
+        File file = new File("maze.png");
+        ImageIO.write(mazePrinter.GetImage(), "png", file);
     }
 }
