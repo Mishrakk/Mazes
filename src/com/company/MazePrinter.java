ef2c8e60d9012e62402c517418b6b37d9f165202
Szymon Murawski <szymon.murawski@gmail.com>
2021-11-08 20:38:01 +0100

    feat: Add DrawCells()

--- src/com/company/MazePrinter.java
+++ src/com/company/MazePrinter.java
@@ -13,12 +13,37 @@ public class MazePrinter {
     public MazePrinter(Grid grid){
         this(grid, 10);
     }
-        BufferedImage bufferedImage = new BufferedImage(CellSize * Grid.Width, CellSize * Grid.Height, BufferedImage.TYPE_INT_RGB);
     public BufferedImage GetImage(){
+        BufferedImage bufferedImage = new BufferedImage(CellSize * Grid.Width + 1, CellSize * Grid.Height + 1, BufferedImage.TYPE_INT_RGB);
         Graphics graphics = bufferedImage.createGraphics();
         SetBackground(graphics);
+        DrawCells(graphics);
+        graphics.dispose();
         return bufferedImage;
     }
+
+    private void DrawCells(Graphics graphics) {
+        graphics.setColor(Color.black);
+        for (Cell cell : Grid){
+            int x1 = cell.X * CellSize;
+            int y1 = cell.Y * CellSize;
+            int x2 = (cell.X + 1) * CellSize;
+            int y2 = (cell.Y + 1) * CellSize;
+            if (cell.North == null){
+                graphics.drawLine(x1, y1, x2, y1);
+            }
+            if (cell.West == null){
+                graphics.drawLine(x1, y1, x1, y2);
+            }
+            if(!cell.Linked(cell.South)){
+                graphics.drawLine(x1, y2, x2, y2);
+            }
+            if(!cell.Linked(cell.East)){
+                graphics.drawLine(x2, y1, x2, y2);
+            }
+        }
+    }
+
     private void SetBackground(Graphics graphics) {
         graphics.setColor(Color.white);
         graphics.fillRect(0, 0, CellSize * Grid.Width, CellSize * Grid.Height);
