2a16d54ee7fb123b90665b5ff0c340cf71de3791
Szymon Murawski <szymon.murawski@gmail.com>
2021-12-16 12:56:46 +0100

    refactor: Delete DistancesGrid

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,9 +8,8 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         System.out.print("Hello World!\n");
-        var grid = new DistancesGrid(100, 100);
+        var grid = new Grid(100, 100);
         SidewinderMaze.generate(grid);
-        grid.distances = grid.getCellAt(0,0).distances;
         System.out.print(grid);
         var mazePrinter = new MazePrinter(grid);
         var file = new File("maze.png");
